745ad14230a97e9f72f930bee6f3143a7c1a3dc7
Fred Ma <fma1@terpmail.umd.edu>
2018-06-14 21:04:33 -0400

    Updated file structure

---
+++ src/main/scala/example/ChapterOne.scala
@@ -0,0 +1,4 @@
+package example
+
+object ChapterOne {
+}
---
+++ src/test/scala/example/ChapterOneSpec.scala
@@ -0,0 +1,9 @@
+package example
+
+import org.scalatest._
+
+class ChapterOneSpec extends FlatSpec with Matchers {
+  "The ChapterOne ..." should "..." in {
+    assert(true)
+  }
+}
