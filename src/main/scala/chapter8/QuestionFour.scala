56349f9738131d3f1dea9161382c381852cc36c5
Fred Ma <fma1@terpmail.umd.edu>
2018-07-11 22:09:11 -0400

    Ch8 #4

--- src/main/scala/chapter8/QuestionFour.scala
+++ src/main/scala/chapter8/QuestionFour.scala
@@ -7,12 +7,32 @@ object QuestionFour {
     private lazy val synth = javax.sound.midi.MidiSystem.getSynthesizer
     private lazy val channel = synth.getChannels.head
 
+    def play(notes: Seq[Int]): Unit = {
+      notes.foreach(note => {
+        channel.noteOn(note, volume)
+        Thread.sleep(250)
+        channel.noteOff(note)
+      })
+    }
+
+    def play(notesLst: List[Seq[Int]]): Unit = notesLst.foreach(notes => play(notes))
+  }
+
+  class NotePlayer2 private(val volume: Int) {
+    private lazy val synth = javax.sound.midi.MidiSystem.getSynthesizer
+    private lazy val channel = synth.getChannels.head
+
     def play(notes: Seq[Int]) = {
       notes.foreach(note => {
         channel.noteOn(note, volume)
         Thread.sleep(250)
         channel.noteOff(note)
-      }
+      })
     }
+  }
+  
+  object NotePlayer2 {
+    lazy val instance = new NotePlayer2(95)
+    def getInstance = instance
   }
 }
